67cc88f720333d34d8c992288902a6d86a36ef13
Mai Trọng Nhân <88148922+StarKevin20022002@users.noreply.github.com>
2022-11-10 14:19:03 +0100

    First ever Java (or, virtually, every lang.) exer.

---
+++ HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+* Problem: Hello World!
+* Link: https://open.kattis.com/problems/hello
+* @author TRONG NHAN MAI
+* @version 1.0, 11/08/2022
+*
+* Method : Ad-Hoc
+* Status : Accepted
+* Runtime: 0.06
+*/
+
+public class HelloWorld {
+    
+    public static void main(String[] args) {
+
+        // Prints "Hello World!" in the terminal window.
+        System.out.println("Hello World!");
+    }
+}
