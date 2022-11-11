f1e157c029d3f0d93de097a30ad6da1d530916e8
Mai Trọng Nhân <88148922+StarKevin20022002@users.noreply.github.com>
2022-11-12 00:33:24 +0100

    Add files via upload

---
+++ GCVWR.java
@@ -0,0 +1,35 @@
+/**
+* Problem: Chanukah Challenge
+* Link: https://open.kattis.com/problems/chanukah
+* @author TRONG NHAN MAI
+* @version 1.0, 11/12/2022
+*
+* Method : Ad-Hoc
+* Status : Accepted
+* Runtime: 0.10
+*/
+
+import java.util.Scanner;
+
+public class GCVWR {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int truck_rating = input.nextInt();
+        int truck_weight = input.nextInt();
+        int item = input.nextInt();
+        int towing_cap = 0;
+        int item_weight = 0;
+        int pull_weight = 0;
+        int i;
+
+        /** calculate pull weight */
+        towing_cap = (truck_rating - truck_weight) * 9 / 10;
+        for(i = 0; i < item; i++) {
+            item_weight += input.nextInt();
+        }
+        pull_weight = towing_cap - item_weight;
+        System.out.println(pull_weight);
+
+        input.close();
+    }
+}
